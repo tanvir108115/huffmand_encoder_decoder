09a5856c4bb9d409292988aa2e99ea86f320be3c
tanvir108115 <75143081+tanvir108115@users.noreply.github.com>
2020-11-28 12:18:30 -0500

    Add files via upload

---
+++ run.py
@@ -0,0 +1,30 @@
+import os
+from app import huffmancoding
+
+
+def clear(): os.system('cls')
+
+clear()
+print("Select the number of task you want to do")
+print(" 1. Compress \n 2. Decompress \n 3. Both compress and decompress")
+x = input("Answer: ")
+library_path = r"library.txt"  # set path of library. By default it is in the same folder as data/compressed file
+
+if x == '1' or x == '3':
+    path = input("Enter the original file path: ")
+    clear()
+    if x == '1':
+        y = huffmancoding(path, library_path,"")
+        y.compress()
+    else:
+        y = huffmancoding(path, library_path,"")
+        y.compress_decompress()
+elif x == '2':
+    path = input("Enter path for binary file:")
+    extension = input("Enter file format:")
+    y = huffmancoding(path, library_path,extension)
+    clear()
+    y.decompress()
+else:
+    print("Invalid selection")
+input("Press any key to exit")
